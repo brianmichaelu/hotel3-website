60250445027108f169dfac38daa089e509046477
brianmichaelu <brianmichaelu@gmail.com>
2026-05-24 11:49:53 +0300

    Update Navigation.tsx

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -26,7 +26,7 @@ export default function Navigation() {
 
     const startPosition = window.pageYOffset;
     const distance = targetPosition - startPosition;
-    const duration = 1000;
+    const duration = 900;
     let startTime: number | null = null;
 
     const animation = (currentTime: number) => {
